319c73f8ed2434a880a8faf88630da6fa47da8f9
Kiril Chuchev <54711535+KirilChuchev@users.noreply.github.com>
2019-09-12 20:25:39 +0300

    Add files via upload

---
+++ 02. Division/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace _02._Division
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int number = int.Parse(Console.ReadLine());
+
+			int counter = 0;
+
+			for (int i = 10; i >= 2; i--)
+			{
+				if (i == 10 || i == 7 || i == 6 || i == 3 || i == 2)
+				{
+					if (number % i == 0)
+					{
+						Console.WriteLine($"The number is divisible by {i}");
+						break;
+
+					}
+					else
+					{
+						counter++;
+
+						if (counter >= 5)
+						{
+							Console.WriteLine("Not divisible");
+							break;
+						}
+						else
+						{
+							continue;
+						}
+						
+					}
+
+					
+				}
+			}
+		}
+	}
+}
---
+++ 5. Login (HOME)/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace _5._Login__HOME_
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string password = Console.ReadLine();
+
+			bool ok = false;
+
+			int counter = 0;
+
+			while (true)
+			{
+				counter++;
+				string reversed = Console.ReadLine();
+
+				for (int i = 0; i < password.Length; i++)
+				{
+					if (password[i] == reversed[password.Length - 1 - i])
+					{
+						Console.WriteLine("User Acer logged in.");
+						ok = true;
+						break;
+					}
+				}
+
+				if (counter == 4)
+				{
+					Console.WriteLine($"User {password} blocked!");
+					break;
+				}
+
+				if (ok)
+				{
+					break;
+				}
+
+				
+			}
+
+		}
+	}
+}
